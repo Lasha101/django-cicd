54a1c02a7001a1a012c24ea58c2695d6f9b9f473
Lasha101 <gurshabo55@gmail.com>
2024-06-12 21:32:31 +0400

    add cicd.yaml file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'tyre321',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
+
+
 
 
 # Password validation
